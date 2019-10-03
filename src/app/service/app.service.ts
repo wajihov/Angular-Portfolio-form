03a2af77957634a6cf9d1f89e8370fd678ebfde0
wajihov <wajihov@hotmail.fr>
2019-10-03 11:38:57 +0100

    Add Service to project

---
+++ src/app/service/app.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+  users = [];
+  constructor() { }
+  addPerson(block: any) {
+      this.users.push(block);
+  }
+
+  deletePerson(i: number) {
+      if (confirm("Are you sure to remove this row?")) {
+          this.users.splice(i, 1);
+      }
+  }
+
+  modifPerson(block: any, i: number) {
+      this.users[i] = block;
+  }
+}
