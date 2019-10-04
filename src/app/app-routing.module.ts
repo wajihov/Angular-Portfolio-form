6a36cad9601b5157964361ccf1d93a0cc163efd7
wajihov <wajihov@hotmail.fr>
2019-10-04 11:48:41 +0100

    Afficher une navbar qui fait le lien avec le routing et non pas avec href

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './portfolio/form/form.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FormComponent },
+  { path: 'contact', component: FormComponent },
+  { path: 'portfolio' , component: PortfolioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
